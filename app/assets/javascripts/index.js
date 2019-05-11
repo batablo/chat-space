755082f3684aec0ebd876a6d386b580cf073878f
batablo <mutemath.us22@gmail.com>
2019-05-11 16:36:58 +0900

    検索時のHTMLの作成
    非同期通信で表示されるようになりましたが、ボタンを押すたびにイベントが発火してしまいます。

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,22 @@
-$(function (){
+$(function() {
+
+  var search_list = $('#user-search-result');
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">${msg}</div>`
+    search_list.append(html);
+  }
 
   $('#user-search-field').on("keyup", function (){
-    var input = $("#user-search-field").val();
-    console.log(input);
+    var input = $('#user-search-field').val();
  
     $.ajax({
       type: 'GET',
@@ -10,5 +24,21 @@ $(function (){
       data: { keyword: input },
       dataType: 'json'
     })
+
+    .done(function(users) {
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーがいません")
+      }
+    })
+    
+    .fail(function() {
+      alert("ユーザ検索に失敗しました。");
+    })
+
   });
 });
