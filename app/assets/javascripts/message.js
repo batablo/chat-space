7c9aa89240ead1d1c1749fe6510cec54e9803f3f
batablo <mutemath.us22@gmail.com>
2019-05-12 15:23:17 +0900

    メッセージ分だけスクロールするよう実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -38,7 +38,7 @@ $(function (){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.form__submit').prop('disabled', false);
-      scroll($('.messages'));
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast')
       $('#new_message')[0].reset();
     })
 
@@ -49,27 +49,19 @@ $(function (){
   });
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data('message-id')
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: 'api/messages',
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       messages.forEach(function(message) {
         insertHTML += buildHTML(message);
-        //メッセージが入ったHTMLを取得
-
-        //メッセージを追加
         $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast')
       }); 
     })
     .fail(function() {
